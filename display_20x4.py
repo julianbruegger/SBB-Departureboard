6569dc5ef6cd1cdc06d564b62285b186568bc62a
Julian Bruegger <julian.bruegger@ict-bz.ch>
2020-06-30 19:13:14 +0200

    Fixed errors

--- display_20x4.py
+++ display_20x4.py
@@ -10,11 +10,28 @@ import requests
 import time
 import math
 import sys
-
+import json
 
 
 while True:
-    from module.fetch_SBB import *
+    url = "http://transport.opendata.ch/v1/stationboard?station=lucerne&limit=1"
+
+    response = requests.get(url)
+    data = response.text
+
+    parsed = json.loads(data)
+
+    station = str(parsed.get("station").get("name"))
+    destination = str(parsed.get("stationboard")[0].get("to"))
+    delay = str(parsed.get("stationboard")[0].get("stop").get("delay"))
+    name = str(parsed.get("stationboard")[0].get("name"))
+    number = str(parsed.get("stationboard")[0].get("number"))
+    category = str(parsed.get("stationboard")[0].get("category"))
+    departure =str(parsed.get("stationboard")[0].get("stop").get("departure"))
+
+    departure_time = departure[-13:16]
+
+
     if delay == 'None':
         if category == 'RE':
             lcd_init()
